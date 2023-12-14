9b4ab51166ac189daa27986651f93a16ea530ef6
Hyoira <yukkurahirokura@gmail.com>
2023-12-14 21:53:26 +0900

    auto reload works

--- charlotte.py
+++ charlotte.py
@@ -2,13 +2,14 @@ import discord
 from discord.ext import tasks
 import config
 import comparator
-
+import asyncio
 
 # トークンの読み込み
 token = config.BOT_TOKEN
 
 # チャンネルIDの読み込み
-channel_id = config.CHANNEL_ID
+# channel_id = config.CHANNEL_ID
+channel_id = '1184446396624011265'
 
 # インテントの生成
 intents = discord.Intents.default()
@@ -17,52 +18,48 @@ intents.message_content = True
 # クライアントの生成
 client = discord.Client(intents=intents)
 
-# if entries_new.empty:
-# print(entries_new["Title"])
-
-"""
-@tasks.loop(seconds=30)
-async def loop():
-    await client.wait_until_ready()
-    channel = client.get_channel(channel_id)
-    await channel.send('時間だよ')
-
-#ループ処理実行
-loop.start()
-
-client.run(token)
-"""
 
 @client.event
-async def on_message(message):
-    if message.author.bot:
-        return
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
 
-    if message.content == '!test':
-        # 新着記事を取得
-        try:
-            comp = comparator.Comparator()
-            entries_new = comp.entries_new
-            await message.channel.send('新着記事のチェックが完了しました!!')
+    channel = await client.fetch_channel(channel_id)
 
-            if not entries_new.empty:
-               entries_new = entries_new.reset_index(drop=True)
-        except: 
-            await message.channel.send('エラーが発生しました。')
-            return
+    if not channel:
+        print(f"指定されたIDのチャンネルが見つかりません: {config.CHANNEL_ID}")
+    else:
+        print(f"チャンネル「{channel.name}」が見つかりました")
+        loop.start()  # チャンネルが見つかったときのみタスクを開始
 
-        if entries_new.empty:
-          await message.channel.send('新着記事はありません')
-          return
+
+@tasks.loop(seconds=60)
+async def loop():
+    await client.wait_until_ready()
+    entries_new = None
+    channel = await client.fetch_channel(channel_id)
+
+    try:
+        comp = comparator.Comparator()
+        entries_new = comp.entries_new
+
+        if not entries_new.empty:
+            entries_new = entries_new.reset_index(drop=True)
+            await channel.send(f'{len(entries_new)}個の新着記事があります!!')
+
+            for index, row in entries_new.iterrows():
+                embed = discord.Embed(
+                    title=row['Title'],
+                    url=row['URL'],
+                    description=row['Summary'],
+                    color=0x00bfff)
+                embed.set_image(url=row['Cover Image'])
+                await channel.send(embed=embed)
         else:
-          await message.channel.send(f'{len(entries_new)}個の新着記事があります!!')
-          for index, row in entries_new.iterrows():
-            embed = discord.Embed(
-               title=row['Title'], 
-               url=row['URL'], 
-               description=row['Summary'], 
-               color=0x00bfff)
-            embed.set_image(url=row['Cover Image'])
-            await message.channel.send(embed=embed)
+            print('新着記事はありません')
+
+    except Exception as e:  # ここで発生した例外の種類とメッセージを出力
+        print(f'エラーが発生しました: {e}')
+        await channel.send('エラーが発生しました。詳細はログを確認してください。')
+
 
 client.run(token)
