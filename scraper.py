1185b37339850873f53433a898a0b2dfb4cd3f43
Hyoira <yukkurahirokura@gmail.com>
2023-12-16 23:15:15 +0900

    Update WebDriver options in scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import chromedriver_binary
+# import chromedriver_binary
 
 class Scraper:
   # WebDriverのパスとオプションを設定
-  driver = webdriver.Chrome()
+  options = webdriver.ChromeOptions()
+  options.add_argument("--no-sandbox")  # サンドボックスモードを無効化するオプション
+  options.add_argument("--headless")    # ヘッドレスモードで実行するオプション
+  driver = webdriver.Chrome(options=options)
 
   # スクレイピングするページのURL
   url = "https://genshin.hoyoverse.com/ja/news/"
